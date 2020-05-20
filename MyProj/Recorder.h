5529c786c0942609680bf26f1b4eb0d75779f3e4
Shahab <Shahab@SHA>
2020-05-20 20:25:33 +0430

    add 'removeFromFile' member function to Recorder

--- MyProj/Recorder.h
+++ MyProj/Recorder.h
@@ -11,7 +11,7 @@
 #include <bits/stdc++.h>
 #include <QException>
 #include <QDebug>
-//#include <string>
+#include <QMessageBox>
 template <class T>
 class Recorder
 {
@@ -49,8 +49,69 @@ public:
 
         delete[] s;
     }
+    static void removeFromFile(T* a)
+    {
+        QDir d(QDir::currentPath() + "/data");
+        try {
+            std::string f = "data/" + std::string(typeid(T).name()).substr(1) + ".txt";
+            int len = f.length();
+            char* s = new char[len];
+            strcpy(s, f.c_str());
+            QString filename = QString(s);
 
-    static QVector<T*> get_dataList();
+            QFile file(filename);
+            if (!file.open(QIODevice::ReadWrite | QIODevice::Text))
+                throw QException();
+
+            QTextStream txt(&file);
+            int flag = 0, num_of_line = 0, lines_count = 0;
+
+            while (!txt.atEnd())
+            {
+                txt.readLine();
+                lines_count++;
+            }
+
+            txt.seek(0);
+
+            while (!txt.atEnd())
+            {
+                QString data_str = txt.readLine();
+                if (data_str == a->get_data())
+                {
+                    flag = 1;
+                    break;
+                }
+                num_of_line++;
+            }
+            if (flag == 1)
+            {
+                file.resize(0);
+                QTextStream out(&file);
+                for (int i = 0; i < lines_count; i++)
+                {
+                    if (i == num_of_line - 1)
+                        continue;
+                    out << Recorder<T>::get_dataList().at(i)->get_data();
+                }
+            }
+            file.flush();
+            file.close();
+
+
+        } catch (QException) {
+            QMessageBox msg;
+            msg.setText("File Not Found!");
+            msg.exec();
+        }
+
+    }
+
+    static QVector<T*> get_dataList()
+    {
+        return Recorder<T>::dataList;
+    }
+
     static void print_dataList()
     {
         for (int i = 0; i < Recorder<T>::dataList.size(); i++)
