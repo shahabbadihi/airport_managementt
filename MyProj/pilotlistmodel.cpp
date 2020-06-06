0d25458b0cec97368c7c78af50c4f35a157026f2
Shahab <Shahab@SHA>
2020-06-06 13:40:25 +0430

    pilotlistmodel added

---
+++ MyProj/pilotlistmodel.cpp
@@ -0,0 +1,6 @@
+#include "pilotlistmodel.h"
+
+PilotListModel::PilotListModel()
+{
+
+}
---
+++ MyProj/pilotlistmodel.h
@@ -0,0 +1,11 @@
+#ifndef PILOTLISTMODEL_H
+#define PILOTLISTMODEL_H
+
+
+class PilotListModel : public MyModel
+{
+public:
+    PilotListModel();
+};
+
+#endif // PILOTLISTMODEL_H
