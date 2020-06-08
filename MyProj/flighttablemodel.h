ab7cd987ae22e30da06d7fe1e0f3fba103769465
arshianajmipour <arshianajmipour@gmail.com>
2020-06-08 20:03:57 +0430

    s

---
+++ MyProj/flighttablemodel.h
@@ -0,0 +1,37 @@
+#ifndef FLIGHTTABLEMODEL_H
+#define FLIGHTTABLEMODEL_H
+
+//#include "mymodel.h"
+#include <QAbstractItemModel>
+#include <QObject>
+#include <QTimer>
+
+class FlightTableModel : public QAbstractItemModel
+{
+    Q_OBJECT
+public:
+    FlightTableModel(QObject *parent = nullptr);
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+    bool insertRows(int row, int count, const QModelIndex &parent) override;
+//    QModelIndex parent(const QModelIndex &index) const override;
+//    QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override;
+//    bool insertRows(int row, int count, const QModelIndex &parent) override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+    static FlightTableModel* getInstance();
+
+private:
+    static FlightTableModel* instance;
+
+    QTimer * timer;
+
+private slots:
+    void timerHit();
+    void recordInserted();
+    //void recordInserted() override;
+};
+
+#endif // FLIGHTTABLEMODEL_H
